479c312b517ed7956f2b3b3abbed43007acd26d3
Sorin Lica <36102092+sorin360@users.noreply.github.com>
2020-02-27 16:30:29 +0200

    Update MyLibrary.swift

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,5 +1,9 @@
+import scsp
+
 struct fdary {
-    var tfext = "Hello,d World!"fd
+    var tfext = "Hello,d World!"
+    
+    func dsd() {
+        let _ = DataManager.instance().getComponentBy(uuid: "")
+    }
 }
-f
-dfv
